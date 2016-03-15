7819f20e2a6e1103b411cf7cb86b21eac64b521e
Thomas <ry386@stud.uni-heidelberg.de>
2016-03-15 16:41:03 +0100

    Separate vigra test.

---
+++ cpp/vigra/main.cpp
@@ -0,0 +1,48 @@
+//#include <vigra/convolution.hxx>
+//#include <vigra/impex.hxx>
+//#include <vigra/imageinfo.hxx>
+//#include <vigra/stdimage.hxx>
+//#include <vigra/multi_array.hxx>
+//#include <vigra/hdf5impex.hxx>
+//#include <vigra/multi_convolution.hxx>
+#include <vigra/multi_blockwise.hxx>
+//#include <vigra/random.hxx>
+
+// uncomment to remove boost dependencies, i.e. time measurements
+// #define NOBOOSTTIMER
+
+#ifndef NOBOOSTTIMER
+#include "boost/timer/timer.hpp"
+#endif
+
+int main(int argc, char** argv){
+  int size = 4096;
+  float sigma = 10.0;
+
+  // load vigra image
+  vigra::MultiArray<2, float> v_img(vigra::Shape2(size,size));
+  for(int i = 0; i < size; i++)
+    for(int j = 0; j < size; j++)
+      v_img(i,j) = float(i*j); //h_img[i+j*size];
+
+  // vigra convolution result
+  vigra::MultiArray<2, float> v_result(vigra::Shape2(size,size));
+
+  vigra::blockwise::BlockwiseConvolutionOptions<2> opt;
+  opt.innerScale(sigma);
+  opt.setNumThreads(8);
+
+#ifndef NOBOOSTTIMER
+  boost::timer::auto_cpu_timer timer;
+  timer.start();
+#endif
+
+  vigra::blockwise::gaussianSmoothMultiArray(v_img, v_result, opt);
+
+#ifndef NOBOOSTTIMER
+  timer.elapsed();
+#endif
+
+  return 0;
+}
+
